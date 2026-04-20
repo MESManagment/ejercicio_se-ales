71473804582b1bd9ac0b2bf50397db8496be13f6
MESManagment <briansoldado12@gmail.com>
2026-04-20 16:04:14 -0300

    Implementacion de button down y grupos

---
+++ ejercicio_senales/scenes/Buttons/VioletGenerator/violet_generator.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _on_button_button_down():
+	generate_block()
+
+func generate_block():
+	# Carga la escena del bloque
+	var block_scene = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
+	
+	# Crea una instancia
+	var instance = block_scene.instantiate()
+	
+	# Se añaden al grupo "blocks"
+	instance.add_to_group("blocks")
+	
+	add_child(instance)
