62f33037dee28df1eec9f11bad4874d0980781cb
MESManagment <briansoldado12@gmail.com>
2026-04-20 16:17:31 -0300

    Implementacion de señales

--- ejercicio_senales/scenes/Buttons/Reset/reset.gd
+++ ejercicio_senales/scenes/Buttons/Reset/reset.gd
@@ -3,3 +3,6 @@ extends TextureButton
 func _on_button_down() -> void:
 	# Busca a todos los objetos que esten en el grupo "blocks"
 	get_tree().call_group("blocks", "delete")
+	
+	# Llama a la funcion delete
+	get_parent().delete()
--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
 const INSTANCES_TEXT = "Cantidad de instancias: "
-# Variable para contar cantidad de instancias
 var instancesCount = 0
 
-func _ready():
-	# Inicializa el texto de "CountLabel" usando el texto
-	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
+# Referencia a los nodos generadores 
+@onready var red_generator = $RedGenerator 
+@onready var violet_generator = $VioletGenerator
+
+func _ready() -> void:
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
-	pass
+	
+	# Conecta la señal "button_down" 
+	red_generator.button_down.connect(count_new_instance)
+	violet_generator.button_down.connect(count_new_instance)
+
+func count_new_instance():
+	instancesCount += 1
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+
+func delete():
+	instancesCount = 0
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
